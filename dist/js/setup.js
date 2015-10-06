794d9db3f73498e3be2693f716d89db2e83ace5f
Kyle Hill <kylehill@gmail.com>
2015-10-06 14:01:51 -0400

    Binds together cueboard/typebox/moousetrap

--- dist/js/setup.js
+++ dist/js/setup.js
@@ -1,5 +1,14 @@
 "use strict";
 
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var caps = letters.map(function (l) {
+  return l.toUpperCase();
+});
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+var extras = ["space", ",", "<", ">", ".", "?", "/", ":", ";", "\"", "'", "{", "[", "}", "]", "|", "\\", "_", "-", "+", "=", "~", "`", "enter", "tab", "delete", "backspace"];
+
+var keys = [].concat(letters, caps, numbers, extras);
+
 var getPage = function getPage(hash, lessonsObject) {
   if (!lessonsObject || !lessonsObject.pageMap) {
     return false;
@@ -12,6 +21,8 @@ var getPage = function getPage(hash, lessonsObject) {
 };
 
 var pageRoute = function pageRoute(context, next) {
+  Mousetrap.reset();
+
   var page = getPage(context.params.hash, App.lessons);
   if (!page || !page.template) {
     return next();
@@ -21,13 +32,38 @@ var pageRoute = function pageRoute(context, next) {
   $(".page-container").html(App.templates[page.template]);
 
   if (page.lesson) {
-    var cb = cueboard(".cueboard-container", {
-      initialKeyState: "inactive"
-    });
+    (function () {
+      var cb = cueboard(".cueboard-container", {
+        initialKeyState: "inactive",
+        keyState: page.keys || {}
+      });
 
-    var tb = typebox(".typebox-container", {
-      string: page.text
-    });
+      var tb = typebox(".typebox-container", {
+        string: page.text
+      });
+
+      var state = tb.state();
+      if (state.next) {
+        cb.changeState("next", state.next);
+      }
+
+      Mousetrap.bind(keys, function (evt, key) {
+        var result = tb.applyCharacter(key);
+
+        if (result.accurate) {
+          cb.changeState("active", key);
+          if (result.next) {
+            cb.changeState("next", result.next);
+          }
+        }
+
+        if (evt.altKey || evt.metaKey || evt.ctrlKey) {
+          return;
+        }
+
+        return false;
+      });
+    })();
   }
 };
 
@@ -39,13 +75,13 @@ $(function () {
 
   // Add structural templates to the page
   $("aside").html(App.templates.sidebar(App.lessons));
-});
 
-// Setup router
-page("/:hash", pageRoute);
-page("/", loadIntro);
+  // Setup router
+  page("/:hash", pageRoute);
+  page("/", loadIntro);
 
-// Start router
-page({
-  hashbang: true
+  // Start router
+  page({
+    hashbang: true
+  });
 });
--- src/js/setup.js
+++ src/js/setup.js
@@ -1,3 +1,10 @@
+const letters = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const caps = letters.map(function(l){ return l.toUpperCase() })
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")" ]
+const extras = ["space", ",", "<", ">", ".", "?", "/", ":", ";", "\"", "'", "{", "[", "}", "]", "|", "\\", "_", "-", "+", "=", "~", "`", "enter", "tab", "delete", "backspace" ]
+
+const keys = [].concat(letters, caps, numbers, extras)
+
 const getPage = function(hash, lessonsObject) {
   if (!lessonsObject || !lessonsObject.pageMap) { return false }
   if (!lessonsObject.pageMap[hash]) { return false }
@@ -6,19 +13,44 @@ const getPage = function(hash, lessonsObject) {
 }
 
 const pageRoute = function(context, next) {
+  Mousetrap.reset()
+
   var page = getPage(context.params.hash, App.lessons)
   if (!page || !page.template) { return next() }
 
   $("main").html(App.templates.page)
   $(".page-container").html(App.templates[page.template])
-
+  
   if (page.lesson) {
-    var cb = cueboard(".cueboard-container", {
-      initialKeyState: "inactive"
+    let cb = cueboard(".cueboard-container", {
+      initialKeyState: "inactive",
+      keyState: page.keys || {}
     })
 
-    var tb = typebox(".typebox-container", {
+    let tb = typebox(".typebox-container", {
       string: page.text
+    })
+
+    const state = tb.state()
+    if (state.next) {
+      cb.changeState("next", state.next)
+    }
+    
+    Mousetrap.bind(keys, function(evt, key){
+      const result = tb.applyCharacter(key)
+      
+      if (result.accurate) {
+        cb.changeState("active", key)
+        if (result.next) {
+          cb.changeState("next", result.next)
+        }
+      }
+
+      if (evt.altKey || evt.metaKey || evt.ctrlKey) {
+        return
+      }
+
+      return false
     })
   }
 }
@@ -32,13 +64,13 @@ $(function(){
   // Add structural templates to the page
   $("aside").html(App.templates.sidebar(App.lessons))
 
-})
-
-// Setup router
+  // Setup router
   page("/:hash", pageRoute)
   page("/", loadIntro)
-  
+
   // Start router
   page({ 
     hashbang: true 
   })
+
+})
